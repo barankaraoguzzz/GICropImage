af8193cd2842ffcc3590c8d72f86cadb5e0ada96
BARAN KARAOĞUZ <baran.karaoguz@consultant.turkcell.com.tr>
2019-05-26 15:31:25 +0300

    Initial Commit

---
+++ GICropImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GICropImage
+//
+//  Created by Baran Karaoğuz on 26.05.2019.
+//  Copyright © 2019 Baran Karaoğuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
