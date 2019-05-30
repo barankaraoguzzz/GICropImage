654da0ef453bf28d677a09b244f08d7c34eb5029
BARAN KARAOĞUZ <baran.karaoguz@ogr.sakarya.edu.tr>
2019-05-30 10:42:31 +0300

    Create imageScaleSize enum

--- GICropImage/Helper/GICropImageCalculater.swift
+++ GICropImage/Helper/GICropImageCalculater.swift
@@ -27,6 +27,43 @@ extension GICropImageCalculater {
     }
 }
 
+
 enum ImageScaleSize {
+    case s32x32
+    case s64x64
+    case s128x128
+    case s256x256
+    case s512x512
+    case s1024x1024
+    case customSize(size: CGFloat)
     
+}
+
+extension ImageScaleSize: RawRepresentable {
+    typealias RawValue = CGSize
+    
+    init?(rawValue: CGSize) {
+        return nil
+    }
+    
+    var rawValue: CGSize {
+        switch self {
+        case .s32x32:
+            return CGSize(width: 32, height: 32)
+        case .s64x64:
+            return CGSize(width: 64, height: 64)
+        case .s128x128:
+            return CGSize(width: 128, height: 128)
+        case .s256x256:
+            return CGSize(width: 256, height: 256)
+        case .s512x512:
+            return CGSize(width: 512, height: 512)
+        case .s1024x1024:
+            return CGSize(width: 1024, height: 1024)
+        case .customSize(let size): do {
+            return CGSize(width: size, height: size)
+            }
+        }
+    }
+
 }
